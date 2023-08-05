00a34b2fdcef33ab8a21bc3540c69e8b4c198128
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-08-05 16:47:55 +0600

    Add files via upload

--- frontend/src/SATValidation.js
+++ frontend/src/SATValidation.js
@@ -1,7 +1,33 @@
 function Validation(values){
     let error = {};
     const email_pat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const password_pat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+    
+
+    if(values.name === "")
+    {
+        error.name = "Name should not be empty";
+    }
+    else
+    {
+        error.name = "";
+    }
+
+    if(values.id === "")
+    {
+        error.id = "ID should not be empty";
+    }
+    else
+    {
+        error.id = "";
+    }
+    if(values.dept === "")
+    {
+        error.dept = "Department should not be empty";
+    }
+    else
+    {
+        error.dept = "";
+    }
 
     if(values.email === "")
     {
@@ -16,17 +42,54 @@ function Validation(values){
         error.email = "";
     }
 
+    var str = JSON.stringify(values.password);
+    var letters =0;
+    var numbers =0;
+    var len = str.length;
+    for(var i =0; i<len ;i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        {
+            numbers++;
+        }
+        else if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+        {
+            letters++;
+        }
+    }
+    len = len -2;
     if(values.password === "")
     {
         error.password = "Password should not be empty";
     }
-    else if(!password_pat.test(values.password))
+    else if(len<8)
     {
-        error.password = "Password didn't match the format";
+        error.password = "Password should contain at least 8 characters.";
+    }
+    else if(numbers === 0)
+    {
+        error.password = "Password must contain at least one digit";
+    }
+    else if(letters === 0)
+    {
+        error.password = "Password must contain at least alphabet";
     }
     else
     {
         error.password = "";
+    }
+
+    if(values.confirmPassword === "")
+    {
+        error.confirmPassword = "Enter the password again.";
+    }
+    else if(JSON.stringify(values.confirmPassword) !== JSON.stringify(values.password))
+    {
+        error.confirmPassword = "Password didn't match.";
+    }
+    else
+    {
+        error.confirmPassword = "";
     }
     return error; 
 }
--- frontend/src/SignUpAsTeacher.js
+++ frontend/src/SignUpAsTeacher.js
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import './stylesSAT.css';
 import LOGO from './LOGO.png';
 import Validation from './SATValidation';
+import axios from 'axios';
 
 function SignUpAsTeacher() {
   const [values, setValues] = useState({
+    name: '',
+    dept: '',
+    id: '',
     email: '',
     password: ''
 })
 
+const navigate = useNavigate();
 const [errors, setErrors] = useState({});
 
 const handleInput=(event) => {
@@ -18,23 +24,40 @@ const handleInput=(event) => {
 const handleSubmit=(event) => {
   event.preventDefault();
   setErrors(Validation(values));
+  if(errors.name === "" && errors.dept === "" && errors.id === "" && errors.email === "" && errors.password === "")
+  {
+    axios.post('http://localhost:7000/SignUpAsTeacher', values)
+    .then(res => {
+      navigate('/Login');
+    })
+    .catch(err => console.log(err))
+  }
 }
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="stylesSAT.css" />
-        <title>Sign Up As Teacher</title>
-      </head>
-      <body>
-        <div id="header">
+    <div>
+      <div id="header">
           <img id="logo" src={LOGO} alt="JU Logo" />
           <h1 id="title">Sign Up As Teacher</h1>
         </div>
+        <form id="signUp-form" action="" onSubmit={handleSubmit}>
+          <label className="text" htmlFor="name">
+            Enter name:
+          </label>
+          <input type="text" id="name" name="name" onChange={handleInput}/><br />
+          {errors.name && <label className="err">{errors.name}</label>}<br/>
 
-        <form id="login-form" action="/Login" method="POST">
+          <label className="text" htmlFor="dept">
+            Enter department:
+          </label>
+          <input type="text" id="dept" name="dept" onChange={handleInput}/><br />
+          {errors.dept && <label className="err">{errors.dept}</label>}<br/>
+
+          <label className="text" htmlFor="id">
+            Enter Identification ID:
+          </label>
+          <input type="text" id="id" name="id" onChange={handleInput}/><br />
+          {errors.id && <label className="err">{errors.id}</label>}<br/>
           <label className="text" htmlFor="email">
             Enter e-mail:
           </label>
@@ -47,19 +70,11 @@ const handleSubmit=(event) => {
           <input type="password" id="password" name="password" onChange={handleInput}/><br />
           {errors.password && <label className="err">{errors.password}</label>}<br/>
 
-          <label className="text">Login as: </label><br />
-          <input type="radio" name="login-type" value="teacher" id="teacher" checked />
-          <label className="text-login-type" htmlFor="teacher">
-            Teacher
-          </label><br />
-          <input type="radio" name="login-type" value="department" />
-          <label className="text-login-type" htmlFor="department">
-            Department
-          </label><br />
-          <input type="radio" name="login-type" value="examControllerOffice" />
-          <label className="text-login-type" htmlFor="examControllerOffice">
-            Exam Controller Office
-          </label><br />
+          <label className="text" htmlFor="confirmPassword">
+            Confirm password:
+          </label>
+          <input type="password" id="confirmPassword" name="confirmPassword" onChange={handleInput}/><br />
+          {errors.confirmPassword && <label className="err">{errors.confirmPassword}</label>}<br/>
 
           <button type="reset" className="buttons" id="reset">
             Reset
@@ -74,8 +89,7 @@ const handleSubmit=(event) => {
           <br />
           Fax:02224491952
         </div>
-      </body>
-    </html>
+    </div>
   );
 }
 
