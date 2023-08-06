1d625a58b90cb1c89312ce4c1fdf205124f8e07f
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-08-06 18:52:56 +0600

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import TeacherDashboard from './TeacherDashboard';
 import SignUpAsTeacher from './SignUpAsTeacher';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
