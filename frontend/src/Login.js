be2138a1dceb242322746d769010b1e5c65a58af
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-08-11 16:59:11 +0600

    Add files via upload
    Added Bootstrap

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -52,51 +52,86 @@ function Login() {
 
    return (
     <div>
-       <div id="header">
-          <img id="logo" src={LOGO} alt="JU Logo" />
-          <h1 id="title">Log-in</h1>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <img className="navbar-brand" id="logo1" src={LOGO} alt="Logo" />
+          <h1 className="navbar-brand-center" id="title">Log-in</h1>
+        </div>
+      </nav>
+      
+      <div className="container-fluid">
+        
+      <div className="row">
+          <div className="col-3"></div>
+          
+          <div className="col-6">
+            <form id="login-form" action="" onSubmit={handleSubmit}>
+            
+              <div className="row">
+                <div className="col-3"></div>
+                <div className="col-6">
+                <div className="row" id="inpt1">
+                  <div className="col-5 text-left">
+                    <label htmlFor="email">Enter e-mail:</label>
+                  </div>
+                  <div className="col-7">
+                    <input type="text" id="email" name="email" placeholder="example@gmail.com" onChange={handleInput}/><br />
+                  </div>
+                  {errors.email && <label className="err">{errors.email}</label>}
+                </div>
+                <div className="row" id="inpt2">
+                  <div className="col-5 text-left">
+                    <label htmlFor="password">Enter password:</label>
+                  </div>
+                  <div className="col-7"><input type="password" id="password" name="password" onChange={handleInput}/><br /></div>
+                  {errors.password && <label className="err">{errors.password}</label>}
+                </div>
+                <div className="row" id="inpt3">
+                  <div className="col-12">
+                    <div className="row lTitle"><label class="text-left">Login as: </label></div>
+                    <div className="row">
+                      <div className="col-1">
+                        <input type="radio" name="loginType" value="teacher" onChange={handleInput}/>
+                      </div>
+                      <div className="col-11"><label class="text-login-type" htmlFor="teacher">Teacher</label></div>
+                    </div>
+                    <div className="row">
+                      <div className="col-1">
+                        <input type="radio" name="loginType" value="department" />
+                      </div>
+                      <div className="col-11"><label class="text-login-type" htmlFor="department">Department</label></div>
+                    </div>
+                    <div className="row">
+                      <div className="col-1">
+                        <input type="radio" name="loginType" value="examControllerOffice" />
+                      </div>
+                      <div className="col-11"><label class="text-login-type" htmlFor="examControllerOffice">Exam Controller Office</label></div>
+                    </div>
+                  </div>
+                  {errors.loginType && <label class="err">{errors.loginType}</label>}
+                </div>
+                <div className="row bn">
+                  <div className="col-6 text-center"><button type="reset" class="buttons" id="reset">Reset</button></div>
+                  <div className="col-6 text-center"><button type="submit" class="buttons" id="submit">Submit</button></div>
+                </div>
+            </div>
+                <div className="col-3"></div>
+              </div>
+            </form>
+          </div>
+
+          <div className="col-3"></div>
         </div>
 
-        <form id="login-form" action="" onSubmit={handleSubmit}>
-          <label className="text" htmlFor="email">
-            Enter e-mail:
-          </label>
-          <input type="text" id="email" name="email" placeholder="example@gmail.com" onChange={handleInput}/><br />
-          {errors.email && <label className="err">{errors.email}</label>}<br/>
-          <label className="text" htmlFor="password">
-            Enter password:
-          </label>
-          <input type="password" id="password" name="password" onChange={handleInput}/><br />
-          {errors.password && <label className="err">{errors.password}</label>}<br/>
-
-          <label className="text">Login as: </label><br />
-          <input type="radio" name="loginType" value="teacher" id="teacher" onChange={handleInput}/>
-          <label className="text-login-type" htmlFor="teacher">
-            Teacher
-          </label><br />
-          <input type="radio" name="loginType" value="department" />
-          <label className="text-login-type" htmlFor="department">
-            Department
-          </label><br />
-          <input type="radio" name="loginType" value="examControllerOffice" />
-          <label className="text-login-type" htmlFor="examControllerOffice">
-            Exam Controller Office
-          </label><br />
-          {errors.loginType && <label className="err">{errors.loginType}</label>}<br/>
-
-          <button type="reset" className="buttons" id="reset">
-            Reset
-          </button>
-          <button type="submit" className="buttons" id="submit">
-            Submit
-          </button>
-        </form>
-        <div id="footer">
-          <hr className="line" />
-          Jahangirnagar University, Savar, Dhaka-1342. Telephone: PABX:02224491045-51,
-          <br />
-          Fax:02224491952
+        <div className="row" id="footer">
+          <div className="col-1"></div>
+          <div className="col-10">
+              <div id="line"></div>
+              Jahangirnagar University, Savar, Dhaka-1342. Telephone: PABX:02224491045-51,<br />Fax:02224491952
+          </div>
+          <div className="col-1"></div>
         </div>
+      </div>
     </div>
   );
 }
