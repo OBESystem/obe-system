799a64e071dc178f4932c18123eb412d052338c4
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-08-15 00:49:25 +0600

    Add files via upload

---
+++ frontend/src/Teacher/CourseFile.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function CourseFile() {
+
+  return (
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default CourseFile
