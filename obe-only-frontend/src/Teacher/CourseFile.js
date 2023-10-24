7119be992e90f75670e4b4b34561ce8dd349acdc
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-10-24 21:36:03 +0600

    Add files via upload

--- obe-only-frontend/src/Teacher/CourseFile.js
+++ obe-only-frontend/src/Teacher/CourseFile.js
@@ -1,12 +1,319 @@
 import React, {useState, useEffect} from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation} from 'react-router-dom';
+//import axios from 'axios';
+import Nav from './Nav';
+import Sidebar from './Sidebar';
+import NA from './NeedAuthentication';
+import './stylesCourseFile.css';
 
 function CourseFile() {
-    const location = useLocation();
-    const data = location.state ? location.state.courseData : null;
+  const location = useLocation();
+  const data = location.state ? location.state.courseData : null;
+
+  const [toggle, setToggle] = useState(true);
+  const [auth, setAuth] = useState(true);
+  const [name, setName] = useState('Dr. Md. Golam Moazzam');
+  const [id, setId] = useState('');
+  const [dept, setDept] = useState('CSE');
+  const Toggle = () =>{
+    setToggle(!toggle)
+  }
+
+  /* useEffect(() => {
+    const classTestButtons = document.querySelectorAll(".btn-classTest");
+
+    classTestButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        // Close all other collapsed elements
+        classTestButtons.forEach((otherButton) => {
+          if (otherButton !== button) {
+            const targetId = otherButton.getAttribute("aria-controls");
+            const target = document.getElementById(targetId);
+            otherButton.setAttribute("aria-expanded", "false");
+            target.classList.remove("show");
+          }
+        });
+
+        // Toggle the aria-expanded attribute and show/hide the content
+        const targetId = button.getAttribute("aria-controls");
+        const target = document.getElementById(targetId);
+        const isExpanded = button.getAttribute("aria-expanded") === "true";
+        button.setAttribute("aria-expanded", isExpanded ? "false" : "true");
+        target.classList.toggle("show");
+      });
+    });
+  }, []); */
+  
+  const handleSubmit=(event) => {
+    event.preventDefault();
+  }
+
+  /*useEffect(()=> {
+    const storedAuth = localStorage.getItem('auth');
+    if (storedAuth === 'true') {
+      setAuth(true);
+      axios.get('http://localhost:7000/TeacherDashboard')
+      .then(res => {
+        if(res.data.Status === "Success")
+        {
+          setAuth(true);
+          localStorage.setItem('auth', 'true');
+          setName(res.data.name);
+          setId(res.data.id);
+          setDept(res.data.dept);
+        }
+      })
+    }
+    else
+    {
+      axios.get('http://localhost:7000/TeacherDashboard')
+      .then(res => {
+        if(res.data.Status === "Success")
+        {
+          setAuth(true);
+          localStorage.setItem('auth', 'true');
+          setName(res.data.name);
+          setId(res.data.id);
+          setDept(res.data.dept);
+        }
+        else
+        {
+          setAuth(false);
+        }
+      })
+    }
+}, [])
+
+  const [info, setInfo] = useState([]);
+
+  const GetInfo=(event) => {
+    axios.get('http://localhost:7000/TeacherInfo', { params: { id: id } })
+    .then(res =>{
+      setInfo(res.data);
+      //console.log("Info of "+ id);
+      //console.log(res.data);
+    })
+    .catch(err => console.log(err));
+  }*/  /* BACKEND PART */
+
+  const navigate = useNavigate();
+  //Reloading removes the user data
+  
+  const [info, setInfo] = useState([]);
+
   return (
-    <div>
-        <spna>{data.courseName}</spna>
+    <div className="container-fluid">
+      {
+         auth ?
+              <div className="container-fluid bg-secondary min-vh-100">
+                <div className="row cnt1">
+                  {toggle && <div className="col-4 col-md-2 bg-white vh-100">
+                    <Sidebar name={name} dept={dept}/>
+                  </div>}
+                  <div className="col">
+                  <div className="px-3">
+                  <Nav Toggle={Toggle} name={name}/>
+                  <div className="container-fluid">
+                      <div className="row g-3 my-2">
+                          <div className="col-md-12 p-1">
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around  rounded">
+                                  <div className="col-md-12 courseInfo">
+                                        <p className="fs-2"><strong>Course title</strong>: <span>{data.courseName}</span></p>
+                                        <p className="fs-5"><strong>Course Code</strong>: <span>{data.courseCode}</span></p>
+                                        <p className="fs-5"><strong>Semester</strong>: <span>{data.year}</span> year <span>{data.semester}</span> semester</p>
+                                        <p className="fs-6"><strong>Type</strong>: <span>{data.courseType}</span></p>
+                                        <p className="fs-6"><strong>Credit</strong>: <span>{data.credit}</span></p>
+                                  </div>
+                              </div>
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around part rounded" id="assignment">
+                                <p>
+                                  <label className="fs-2"><strong>Assesment:</strong></label>
+                                </p>
+                                <div>
+                                  <div className="card card-body">
+                                    <form onSubmit={handleSubmit}>
+                                      <div className="mb-3">
+                                        <label for="assignmentTitle" className="form-label infoFormTitle">Title:</label>
+                                        <input className="form-control" name="assignmentTitle" type="text" id="assignmentTitle" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="assignmentCp" className="form-label infoFormTitle">Compliance Form:</label>
+                                        <input className="form-control" name="complianceFormAssignment" type="file" id="assignmentCp" required/>
+                                      </div>
+                                      <button type="reset" class="btn btn-secondary">Reset</button>
+                                      <button type="submit" class="btn btn-success btnSubmit">Submit</button>
+                                      <button type="submit" class="btn btn-lg btn-info btn-enterMarks">Enter Marks</button>
+                                    </form>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around part rounded" id="tutorial">
+                                <p>
+                                  <label className="fs-2"><strong>Class tests:</strong></label>
+                                  <div className="btn-group">
+                                    <button className="dropdown-toggle" id="btn-chooseClassTest" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                      Select one
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                      <li>
+                                      <button className="btn-classTest" id="btn-cT1" type="button" data-bs-toggle="collapse" data-bs-target="#classTest1" aria-expanded="false" aria-controls="collapseExample">
+                                         Class test 1
+                                      </button>
+                                      </li>
+                                      <li>
+                                      <button className="btn-classTest" id="btn-cT2" type="button" data-bs-toggle="collapse" data-bs-target="#classTest2" aria-expanded="false" aria-controls="collapseExample">
+                                        Class test 2
+                                      </button>
+                                      </li>
+                                      <li>
+                                      <button className="btn-classTest" id="btn-cT3" type="button" data-bs-toggle="collapse" data-bs-target="#classTest3" aria-expanded="false" aria-controls="collapseExample">
+                                        Class test 3
+                                      </button>
+                                      </li>
+                                    </ul>
+                                  </div>
+                                </p>
+                                <div className="collapse" id="classTest1">
+                                  <div className="card card-body">
+                                    <label id="courseFileClassTestTitle">Class Test 1</label>
+                                    <form onSubmit={handleSubmit}>
+                                      <div className="mb-3">
+                                        <label for="q1" className="form-label infoFormTitle">Question:</label>
+                                        <input className="form-control" name="questionCT1" type="file" id="q1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="cp1" className="form-label infoFormTitle">Compliance Form:</label>
+                                        <input className="form-control" name="complianceFormCT1" type="file" id="cp1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH1" className="form-label infoFormTitle">Answer Script for Highest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptBestCT1" type="file" id="ansH1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH1" className="form-label infoFormTitle">Answer Script for Average Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptAvgCT1" type="file" id="ansA1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansL1" className="form-label infoFormTitle">Answer Script for Lowest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptLowCT1" type="file" id="ansL1" required/>
+                                      </div>
+                                      <button type="reset" class="btn btn-secondary">Reset</button>
+                                      <button type="submit" class="btn btn-success btnSubmit">Submit</button>
+                                      <button type="submit" class="btn btn-lg btn-info btn-enterMarks">Enter Marks</button>
+                                    </form>
+                                  </div>
+                                </div>
+                                <div className="collapse" id="classTest2">
+                                  <div className="card card-body">
+                                    <label id="courseFileClassTestTitle">Class Test 2</label>
+                                    <form onSubmit={handleSubmit}>
+                                      <div className="mb-3">
+                                        <label for="q2" className="form-label infoFormTitle">Question:</label>
+                                        <input className="form-control" name="questionCT2" type="file" id="q2" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="cp2" className="form-label infoFormTitle">Compliance Form:</label>
+                                        <input className="form-control" name="complianceFormCT2" type="file" id="cp2" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH2" className="form-label infoFormTitle">Answer Script for Highest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptBestCT2" type="file" id="ansH2" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH2" className="form-label infoFormTitle">Answer Script for Average Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptAvgCT2" type="file" id="ansA2" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansL2" className="form-label infoFormTitle">Answer Script for Lowest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptLowCT2" type="file" id="ansL2" required/>
+                                      </div>
+                                      <button type="reset" class="btn btn-secondary">Reset</button>
+                                      <button type="submit" class="btn btn-success btnSubmit">Submit</button>
+                                      <button type="submit" class="btn btn-lg btn-info btn-enterMarks">Enter Marks</button>
+                                    </form>
+                                  </div>
+                                </div>
+                                <div className="collapse" id="classTest3">
+                                  <div className="card card-body">
+                                  <label id="courseFileClassTestTitle">Class Test 3</label>
+                                    <form onSubmit={handleSubmit}>
+                                      <div className="mb-3">
+                                        <label for="q3" className="form-label infoFormTitle">Question:</label>
+                                        <input className="form-control" name="questionCT3" type="file" id="q3" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="cp3" className="form-label infoFormTitle">Compliance Form:</label>
+                                        <input className="form-control" name="complianceFormCT3" type="file" id="cp3" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH3" className="form-label infoFormTitle">Answer Script for Highest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptBestCT3" type="file" id="ansH3" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansH3" className="form-label infoFormTitle">Answer Script for Average Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptAvgCT3" type="file" id="ansA3" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="ansL3" className="form-label infoFormTitle">Answer Script for Lowest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptLowCT3" type="file" id="ansL3" required/>
+                                      </div>
+                                      <button type="reset" class="btn btn-secondary">Reset</button>
+                                      <button type="submit" class="btn btn-success btnSubmit">Submit</button>
+                                      <button type="submit" class="btn btn-lg btn-info btn-enterMarks">Enter Marks</button>
+                                    </form>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around part rounded" id="finalExam">
+                                <p>
+                                  <label className="fs-2"><strong>Final exam:</strong></label>
+                                </p>
+                                <div>
+                                  <div className="card card-body">
+                                    <form onSubmit={handleSubmit}>
+                                      <div className="mb-3">
+                                        <label for="finaLq1" className="form-label infoFormTitle">Question:</label>
+                                        <input className="form-control" name="questionFinal1" type="file" id="finaLq1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="finaLcp1" className="form-label infoFormTitle">Compliance Form:</label>
+                                        <input className="form-control" name="complianceFormFinal1" type="file" id="finaLcp1" required/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="finaLansH1" className="form-label infoFormTitle">Answer Script for Highest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptBestFinal1" type="file" id="finaLansH1"/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="finaLansH1" className="form-label infoFormTitle">Answer Script for Average Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptAvgFinal1" type="file" id="finaLansA1"/>
+                                      </div>
+                                      <div className="mb-3">
+                                        <label for="finaLansL1" className="form-label infoFormTitle">Answer Script for Lowest Marks Attained:</label>
+                                        <input className="form-control" name="answerscriptLowFinal1" type="file" id="finaLansL1"/>
+                                      </div>
+                                      <button type="reset" class="btn btn-secondary">Reset</button>
+                                      <button type="submit" class="btn btn-success btnSubmit">Submit</button>
+                                      <button type="submit" class="btn btn-lg btn-info btn-enterMarks">Enter Marks</button>
+                                    </form>
+                                  </div>
+                                </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="row g-3 my-2">
+                       
+                      </div>
+                  </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        :
+
+          <div>
+            <NA />
+          </div>
+      }
     </div>
   )
 }
