ed5af11055f3b71a4d6a63b9e97fdd7b3784a739
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-10-26 20:47:13 +0600

    Add files via upload

--- obe-only-frontend/src/Teacher/TeacherDashboard.js
+++ obe-only-frontend/src/Teacher/TeacherDashboard.js
@@ -80,7 +80,7 @@ function TeacherDashboard() {
          auth ?
               <div className="container-fluid bg-secondary min-vh-100">
                 <div className="row cnt">
-                  {toggle && <div className="col-4 col-md-2 bg-white">
+                  {toggle && <div className="col-4 col-md-2 bg-white vh-100">
                     <Sidebar name={name} dept={dept}/>
                   </div>}
                   <div className="col">
