a9d472a000aa12bb27af138b770080c40729b229
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-10-27 00:18:26 +0600

    Add files via upload

--- obe-only-frontend/src/Teacher/Nav.js
+++ obe-only-frontend/src/Teacher/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Nav(props) {
   return (
@@ -7,12 +7,11 @@ function Nav(props) {
         <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
             aria-expanded="false" aria-label="Toggle navigation">
                 <i className="bi bi-justify"></i>
-            </button>
+        </button>
         <div className="collapse navbar-collapse" id="collapsibleNavId">
             <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
                 <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="dropdownId" 
-                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {props.name}
                     </a>
                     <div className="dropdown-menu" aria-labelledby="dropdownId">
--- obe-only-frontend/src/Teacher/TeacherDashboard.js
+++ obe-only-frontend/src/Teacher/TeacherDashboard.js
@@ -75,15 +75,16 @@ function TeacherDashboard() {
     navigate('/Enter-Course-File', { state: { courseData: info[index] } });
   }
   return (
-    <div className="container-fluid">
+    <div className="container-fluid cnt">
       {
          auth ?
-              <div className="container-fluid bg-secondary min-vh-100">
-                <div className="row cnt">
-                  {toggle && <div className="col-4 col-md-2 bg-white vh-100">
-                    <Sidebar name={name} dept={dept}/>
-                  </div>}
-                  <div className="col">
+          <div className="container-fluid bg-secondary min-vh-100">
+            {toggle && 
+            <div className="cnt row">
+              <div className="col-2 bg-white ">
+                <Sidebar name={name} dept={dept}/>
+              </div>
+              <div className="col-10 vh-100">
                   <div className="px-3">
                   <Nav Toggle={Toggle} name={name}/>
                   <div className="container-fluid">
@@ -150,7 +151,76 @@ function TeacherDashboard() {
                   </div>
               </div>
               </div>
+            </div>}
+            {!toggle && 
+            <div className="cnt vh-100">
+                <div className="px-3">
+                <Nav Toggle={Toggle} name={name}/>
+                <div className="container-fluid">
+                    <div className="row g-3 my-2">
+                        <div className="col-md-3 p-1">
+                            <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 className="fs-2">2</h3>
+                                    <p className="fs-5">Due Course File Submissions</p>
+                                </div>
+                                <i className="bi bi-card-list p-3 fs-1"></i>
+                            </div>
+                        </div>
+                        <div className="col-md-3 p-1">
+                            <div className="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 className="fs-2">3</h3>
+                                    <p className="fs-5">Due Marks Submissions</p>
+                                </div>
+                                <i className="bi bi-clock p-3 fs-1"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row g-3 my-2">
+                      <button className="btn btn-primary btn-outline-light btn-lg lButton" 
+                      onClick={() => {
+                        setShowCourseList(!showCourseList);
+                        //GetInfo();
+                      }}>Show the list of Courses</button>
+                    </div>
+                    {showCourseList && (<div className="row g-3 my-2">
+                    <strong><span className="fs-3">List of courses:</span></strong>
+                    <table className="table table-responsive-sm table-striped table-bordered bg-white text-center">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Course Title</th>
+                          <th scope="col">Course Code</th>
+                          <th scope="col">Year</th>
+                          <th scope="col">Semester</th>
+                          <th scope="col">Course type</th>
+                          <th scope="col">Credit</th>
+                          <th scope="col">Link</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      {info.slice(0, info.length).map((course,index) => {
+                      return (
+                        <tr>
+                            <td>{index+1}</td>
+                            <td>{course.courseName}</td>
+                            <td>{course.courseCode}</td>
+                            <td>{course.year}</td>
+                            <td>{course.semester}</td>
+                            <td>{course.courseType}</td>
+                            <td>{course.credit}</td>
+                            <td><button className="btn btn-secondary btn-outline-light" onClick={() => EnterCF(index)}>Course File</button></td>
+                        </tr>
+                      );
+                      })}
+                      </tbody>
+                    </table>
+                    </div>)}
+                </div>
             </div>
+            </div>
+            } 
           </div>
         :
 
