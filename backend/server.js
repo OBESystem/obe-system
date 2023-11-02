1eec46052acb45ab43b2070068409ca6d60864e4
SubarnaSaha08 <69090551+SubarnaSaha08@users.noreply.github.com>
2023-11-03 00:39:24 +0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -39,8 +39,9 @@ const verifyUser = (req, res, next) => {
             else
             {
                 req.name= decoded.name;
-                req.id = decoded.id;
+                req.user_id = decoded.user_id;
                 req.dept = decoded.dept;
+                req.designation=decoded.designation;
                 next();
             }
         })
@@ -48,11 +49,11 @@ const verifyUser = (req, res, next) => {
 }
 
 app.get('/TeacherDashboard', verifyUser, (req, res) => {
-    return res.json({Status: "Success", name: req.name, id: req.id, dept: req.dept});
+    return res.json({Status: "Success", user_id: req.user_id, name: req.name, id: req.user_id, dept: req.dept, designation: req.designation});
 })
 
-app.get('/TeacherInfo', (req, res) => {
-    const sql = "SELECT * FROM coursetable WHERE `t_id` = ?";
+app.get('/CourseInfo', (req, res) => {
+    const sql = "SELECT * FROM coursetable WHERE `t_id` = (SELECT t_id FROM teacher WHERE user_id = ?)";
     const values = [req.query.id];
 
     db.query(sql, [values], (err, result)=> {
@@ -64,14 +65,85 @@ app.get('/TeacherInfo', (req, res) => {
     })
 })
 
-app.post('/SignUpAsTeacher', (req, res)=> {
-    const sql = "INSERT INTO teachers (`name`, `department`, `t_id`, `email`, `password`) values (?)";
+app.get('/CTInfo', (req, res) => {
+    const sql = "SELECT * FROM classtests WHERE `courseCode` = ? AND `examYear`= ?";
+
+    db.query(sql, [req.query.courseCode, req.query.examYear], (err, result)=> {
+        if(err)
+        {
+            return res.json("Error..");
+        }
+        return res.json(result);
+    })
+})
+
+app.post('/AddCT', (req, res)=> {
+    const sql = "INSERT INTO classtests (`courseCode`, `examYear`, `ctID`) values (?)";
+    const values = [
+        req.body.courseCode,
+        req.body.examYear,
+        req.body.ctID
+    ];
+
+    db.query(sql, [values], (err, data)=> {
+        if(err){
+            console.log(req.body.courseCode+" - "+req.body.examYear+" "+req.body.ctID);
+            return res.json("Error");
+        }
+        return res.json("Success");
+    })
+})
+
+app.get('/AGNInfo', (req, res) => {
+    const sql = "SELECT * FROM assignment WHERE `courseCode` = ? AND `examYear`= ?";
+
+    db.query(sql, [req.query.courseCode, req.query.examYear], (err, result)=> {
+        if(err)
+        {
+            return res.json("Error..");
+        }
+        return res.json(result);
+    })
+})
+
+app.post('/AddAGN', (req, res)=> {
+    const sql = "INSERT INTO assignment (`courseCode`, `examYear`, `agnID`) values (?)";
+    const values = [
+        req.body.courseCode,
+        req.body.examYear,
+        req.body.agnID
+    ];
+
+    db.query(sql, [values], (err, data)=> {
+        if(err){
+            //console.log(req.body.courseCode+" - "+req.body.examYear+" "+req.body.ctID);
+            return res.json("Error");
+        }
+        return res.json("Success");
+    })
+})
+
+app.post('/SubmitCourseFile', (req, res)=> {
+    const sql = "UPDATE coursetable SET `isCourseFileSubmitted`= ? WHERE `courseCode` = ? AND `examYear` = ?";
+
+    db.query(sql, ['1',req.body.courseCode,req.body.examYear], (err, data)=> {
+        if(err){
+            return res.json("Error");
+        }
+        return res.json("Success");
+    })
+})
+
+app.post('/SignUp', (req, res)=> {
+    const sql = "INSERT INTO user (`name`, `department`, `designation`, `email`, `phoneNumber`, `password`, `userType`) values (?)";
     const values = [
         req.body.name,
         req.body.dept,
-        req.body.id,
+        req.body.designation,
         req.body.email,
-        req.body.password
+        req.body.phoneNumber,
+        req.body.password,
+        0
     ];
 
     db.query(sql, [values], (err, data)=> {
@@ -83,42 +155,37 @@ app.post('/SignUpAsTeacher', (req, res)=> {
 })
 
 app.post('/Login', (req, res)=> {
-    
-    var type = JSON.stringify(req.body.loginType);
-    var l = type.length;
-    type = type.slice(2, l -2);
-    if(type === "teacher")
+    const sql = "SELECT * FROM user WHERE `email` = ?";
+    db.query(sql, [req.body.email], (err, data)=> {
+    if(err){
+         return res.json("Error");
+    }
+    if(data.length == 0)
     {
-        const sql = "SELECT * FROM teachers WHERE `email` = ?";
-        db.query(sql, [req.body.email,], (err, data)=> {
-        if(err){
-            return res.json("Error");
-        }
-        if(data.length == 0)
+        return res.json("Incorrect email!!");
+    }
+    else
+    {
+        var str = JSON.stringify(req.body.password);
+        var len = str.length;
+        str = str.slice(2, len -2);
+        if(data[0].password === str)
         {
-            return res.json("Incorrect email!!");
+            const user_id = data[0].user_id;
+            const name = data[0].name;
+            const designation = data[0].designation;
+            const userType = data[0].userType;
+            const dept = data[0].department;
+            const token = jwt.sign({ user_id, name, userType, dept, designation },"jsonwebtoken-secret-key", {expiresIn: '1d'});
+            res.cookie('token', token);
+            return res.json(userType);
         }
         else
         {
-            var str = JSON.stringify(req.body.password);
-            var len = str.length;
-            str = str.slice(2, len -2);
-            if(data[0].password === str)
-            {
-                const name = data[0].name;
-                const id = data[0].t_id;
-                const dept = data[0].department;
-                const token = jwt.sign({ name, id, dept },"jsonwebtoken-secret-key", {expiresIn: '1d'});
-                res.cookie('token', token);
-                return res.json("Successful..");
-            }
-            else
-            {
-                return res.json("Incorrect password!!");
-            }
+            return res.json("Incorrect password!!");
         }
-       })
     }
+    }) 
 })
 
 
