61602d679c18827e92fff34de361e5dfe6d070de
Rubayed All Islam (Munna) <66661757+RubayedMunna@users.noreply.github.com>
2023-11-02 22:56:22 +0600

    Add files via upload

---
+++ obe-only-frontend/src/DeptAdmin/ApproveTeacher.jsx
@@ -0,0 +1,216 @@
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Nav from './NavAdmin';
+import Sidebar from './AdminSidebar';
+import NA from './NeedAuthentication';
+import './stylesApproveTeacher.css';
+
+function ApproveTeacher() {
+//   const location = useLocation();
+//   const data = location.state ? location.state.courseData : null;
+
+  const [toggle, setToggle] = useState(true);
+  const [auth, setAuth] = useState(true);
+  const [name, setName] = useState('Dr. Md. Golam Moazzam');
+  const [id, setId] = useState('');
+  const [dept, setDept] = useState('CSE');
+  const Toggle = () =>{
+    setToggle(!toggle)
+  }
+  
+  const handleSubmit=(event) => {
+    event.preventDefault();
+  }
+
+  /*useEffect(()=> {
+    const storedAuth = localStorage.getItem('auth');
+    if (storedAuth === 'true') {
+      setAuth(true);
+      axios.get('http://localhost:7000/TeacherDashboard')
+      .then(res => {
+        if(res.data.Status === "Success")
+        {
+          setAuth(true);
+          localStorage.setItem('auth', 'true');
+          setName(res.data.name);
+          setId(res.data.id);
+          setDept(res.data.dept);
+        }
+      })
+    }
+    else
+    {
+      axios.get('http://localhost:7000/TeacherDashboard')
+      .then(res => {
+        if(res.data.Status === "Success")
+        {
+          setAuth(true);
+          localStorage.setItem('auth', 'true');
+          setName(res.data.name);
+          setId(res.data.id);
+          setDept(res.data.dept);
+        }
+        else
+        {
+          setAuth(false);
+        }
+      })
+    }
+}, [])
+
+  const [info, setInfo] = useState([]);
+
+  const GetInfo=(event) => {
+    axios.get('http://localhost:7000/TeacherInfo', { params: { id: id } })
+    .then(res =>{
+      setInfo(res.data);
+      //console.log("Info of "+ id);
+      //console.log(res.data);
+    })
+    .catch(err => console.log(err));
+  }*/  /* BACKEND PART */
+
+  const navigate = useNavigate();
+  //Reloading removes the user data
+  
+  const [info, setInfo] = useState([]);
+
+  return (
+    <div className="cnt1 container-fluid">
+      {
+         auth ?
+          <div className="container-fluid bg-secondary min-vh-100">
+            {toggle && 
+            <div className="cnt1 row">
+              <div className="col-2 bg-white">
+                <Sidebar name={name} dept={dept}/>
+              </div>
+              <div className="col-10">
+                <div className="px-3">
+                  <Nav Toggle={Toggle} name={name}/>
+                  <div className="container-fluid" id="courseFile">
+                    <div className="row g-3 my-2">
+                      <div className="col-md-12 p-1">
+                        <div className="row p-3 bg-white shadow-sm d-flex justify-content-around rounded">
+                          <div className="col-md-12 courseInfo">
+                            <p className="fs-2"><strong>Department</strong>: <span>{"Computer Science and Engineering"}</span></p>
+                            <p className="fs-3"><strong>Approve Pending Teacher Account</strong></p>
+                            
+                          </div>
+                        </div>
+                        <div className="row p-3 bg-white shadow-sm d-flex justify-content-around part rounded" id="assignment">
+                          <p>
+                            <button className="btn btn-primary btn-outline-light lButton" 
+                            onClick={() => {
+                              // setShowCourseList(!showCourseList);
+                              // GetInfo();
+                            }}>Show Pending Teacher Accounts</button>
+                          </p>
+                          <div>
+                            <table className='table table-striped table-hover teacherTable'>
+                              <thead>
+                                <th>User ID</th>
+                                <th>Name</th>
+                                <th>Designation</th>
+                                <th>Department</th>
+                                <th>Email</th>
+                                <th>Phone Number</th>
+                                <th colSpan={2}>Action</th>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>1</td>
+                                  <td>Rubayed All Islam</td>
+                                  <td>Professor</td>
+                                  <td>Computer Science and Engineering</td>
+                                  <td>rubayed@gmail.com</td>
+                                  <td>+8801864852588</td>
+                                  <td><button type="button" class="btn btn-primary">Approve</button></td>
+                                  <td><button type="button" class="btn btn-danger">Reject</button></td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                        
+                        
+                      </div>
+                    </div>
+                    <div className="row g-3 my-2">
+                    
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+
+                  {!toggle && 
+                  <div className="cnt1">
+                  <div className="px-3">
+                  <Nav Toggle={Toggle} name={name}/>
+                  <div className="container-fluid" id="courseFile">
+                      <div className="row g-3 my-2">
+                          <div className="col-md-12 p-1">
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around rounded">
+                                  <div className="col-md-12 courseInfo">
+                                    <p className="fs-2"><strong>Department</strong>: <span>{"Computer Science and Engineering"}</span></p>
+                                    <p className="fs-3"><strong>Approve Pending Teacher Account</strong></p>
+                                  </div>
+                              </div>
+                              <div className="row p-3 bg-white shadow-sm d-flex justify-content-around part rounded" id="assignment"> 
+                              <p>
+                                <button className="btn btn-primary btn-outline-light lButton" 
+                                onClick={() => {
+                                  // setShowCourseList(!showCourseList);
+                                  // GetInfo();
+                                }}>Show Pending Teacher Accounts</button>
+                              </p>
+                                <div>
+                                  <table className='table table-striped table-hover teacherTable'>
+                                    <thead>
+                                      <th>User ID</th>
+                                      <th>Name</th>
+                                      <th>Designation</th>
+                                      <th>Department</th>
+                                      <th>Email</th>
+                                      <th>Phone Number</th>
+                                      <th colSpan={2}>Action</th>
+                                    </thead>
+                                    <tbody>
+                                      <tr>
+                                        <td>1</td>
+                                        <td>Rubayed All Islam</td>
+                                        <td>Professor</td>
+                                        <td>Computer Science and Engineering</td>
+                                        <td>rubayed@gmail.com</td>
+                                        <td>+8801864852588</td>
+                                        <td><button type="button" class="btn btn-primary">Approve</button></td>
+                                        <td><button type="button" class="btn btn-danger">Reject</button></td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </div>
+                              </div>
+                              
+                              
+                          </div>
+                      </div>
+                      <div className="row g-3 my-2">
+                       
+                      </div>
+                  </div>
+              </div>
+              </div>}
+          </div>
+        :
+          <div>
+            <NA />
+          </div>
+      }
+    </div>
+  )
+}
+
+export default ApproveTeacher;
