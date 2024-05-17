8eec9f0e05e5ddb699f5612ebd5246b99184e1b9
Subarna Saha <69090551+SubarnaSaha08@users.noreply.github.com>
2024-05-17 21:36:19 +0600

    Delete obe-system(React+Django)/backend/demo directory

--- obe-system(React+Django)/backend/demo/academicCalendar/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from academicCalendar.views import AcademicCalendarView
-
-urlpatterns = [
-    path('', AcademicCalendarView.as_view({'get': 'list'}), name='academiccalendar'),
-]
